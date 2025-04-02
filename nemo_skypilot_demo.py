4462784f4fc0c5e34d192aef2cb323eefe1d19ef
hpe <hpe@utility01.tf-ai.io>
2025-04-02 14:19:52 +0900

    update parameter

--- nemo_skypilot_demo.py
+++ nemo_skypilot_demo.py
@@ -17,14 +17,14 @@ def configure_fn():
     return fn
 
 
-def configure_recipe(nodes: int = 1, gpus_per_node: int = 2):
+def configure_recipe(gpus_per_node, num_nodes):
     recipe = llm.nemotron3_4b.pretrain_recipe(
         dir="/checkpoints/nemotron",  # Path to store checkpoints
         name="nemotron_pretraining",
-        tensor_parallelism=2,
-        num_nodes=nodes,
+        tensor_parallelism=gpus_per_node,
+        num_nodes=num_nodes,
         num_gpus_per_node=gpus_per_node,
-        max_steps=100,  # Setting a small value for the quickstart
+        max_steps=2,  # Setting a small value for the quickstart
     )
 
     # Add overrides here
@@ -37,7 +37,7 @@ def common_envs():
     return {}
 
 
-def skypilot_executor(container_image: str, gpus_per_node: int = 2):
+def skypilot_executor(container_image, gpus_per_node):
     return run.SkypilotExecutor(
         gpus="L40S",  # sky show-gpus コマンドで確認
         gpus_per_node=gpus_per_node,
@@ -45,7 +45,7 @@ def skypilot_executor(container_image: str, gpus_per_node: int = 2):
         container_image=container_image,
         cloud="kubernetes",
         cluster_name="nemo_demo",
-        file_mounts={"/nemo_app": "/app"},  # なにかマウントしておかないと、/nemo_runもマウント失敗しているよう。
+        file_mounts={"/nemo_app": "/app"},  # なにかマウントしておかないと、/nemo_runのマウントも失敗しているよう。
         setup="""
         conda deactivate
         nvidia-smi
@@ -54,11 +54,15 @@ def skypilot_executor(container_image: str, gpus_per_node: int = 2):
 
 
 def main():
-    fn = configure_fn()
-    # recipe = configure_recipe()
-    executor = skypilot_executor(container_image="imokuri123/nemo-executor:v0.0.5")
+    gpus_per_node = 4
+    num_nodes = 1
 
-    run.run(fn, executor=executor, name="nemotron3_4b_pretraining")
+    # fn = configure_fn()
+    recipe = configure_recipe(gpus_per_node, num_nodes)
+    executor = skypilot_executor("imokuri123/nemo-executor:v0.0.5", gpus_per_node)
+
+    # run.run(fn, executor=executor, name="add_object")
+    run.run(recipe, executor=executor, name="nemotron3_4b_pretraining")
 
 
 if __name__ == "__main__":
